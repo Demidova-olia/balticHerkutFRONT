9a5a7feeaf4e1320c4f80f3cfba644abd1eed853
Demidova.olia <demidova.olia@gmail.com>
2025-05-08 08:22:28 +0300

    save

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,16 +1,16 @@
-import axios, { AxiosError, InternalAxiosRequestConfig, } from "axios";
+import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000/api",
+  // baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000/api",
+  baseURL: "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Request interceptor
 axiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    const token = localStorage.getItem("token");
+    const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
     if (token && config.headers) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -21,13 +21,15 @@ axiosInstance.interceptors.request.use(
   }
 );
 
-// Response interceptor
 axiosInstance.interceptors.response.use(
-  (response) => response,
+  (response: AxiosResponse) => response,
   (error: AxiosError) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      window.location.href = "/login";
+   
+      if (typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        window.location.href = "/login"; 
+      }
     }
     return Promise.reject(error);
   }
